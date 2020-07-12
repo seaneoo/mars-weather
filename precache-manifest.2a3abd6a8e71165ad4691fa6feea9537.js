2fc28772410c3632028c6696e682bae116901bec
Sean O'Connor <seanoconnor941@gmail.com>
2020-07-12 11:27:22 -0600

    Updates

---
+++ precache-manifest.2a3abd6a8e71165ad4691fa6feea9537.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "213b589a0854d50a29044a8326f116ad",
+    "url": "/mars-weather/index.html"
+  },
+  {
+    "revision": "5206fbe4745b429521eb",
+    "url": "/mars-weather/static/css/main.b043a946.chunk.css"
+  },
+  {
+    "revision": "aaa301ed8925d67db71b",
+    "url": "/mars-weather/static/js/2.95702d66.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mars-weather/static/js/2.95702d66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5206fbe4745b429521eb",
+    "url": "/mars-weather/static/js/main.46fd179e.chunk.js"
+  },
+  {
+    "revision": "b167cab54b44394e6ce8",
+    "url": "/mars-weather/static/js/runtime-main.6eb27c67.js"
+  }
+]);
