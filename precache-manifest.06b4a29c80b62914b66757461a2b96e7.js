7cbc1fc9b21a311b9325e9e55659d810fc500e59
Sean O'Connor <seanoconnor941@gmail.com>
2020-12-05 20:57:17 -0700

    Updates

---
+++ precache-manifest.06b4a29c80b62914b66757461a2b96e7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "755b978857fbc6d64f87162de9321e58",
+    "url": "/mars-weather/index.html"
+  },
+  {
+    "revision": "bf64ee62831384f4bbab",
+    "url": "/mars-weather/static/js/2.612e004a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/mars-weather/static/js/2.612e004a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d31d429a71609eb43525",
+    "url": "/mars-weather/static/js/main.64311c77.chunk.js"
+  },
+  {
+    "revision": "b167cab54b44394e6ce8",
+    "url": "/mars-weather/static/js/runtime-main.6eb27c67.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mars-weather/precache-manifest.aeaa7daf15eb6e008464d40029c75c9e.js"
+  "/mars-weather/precache-manifest.06b4a29c80b62914b66757461a2b96e7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mars-weather/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
