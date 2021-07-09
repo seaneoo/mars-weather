a2f796eea21f03a7d38b009f1d7e38db84cd1d14
Sean O'Connor <seanoconnor941@gmail.com>
2021-07-09 13:15:48 -0600

    Add dark mode

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,12 @@
-import { useEffect, useState } from "react";
-import { Grid, Heading } from "@chakra-ui/react";
+import { Fragment, useEffect, useState } from "react";
+import { Button, Grid, Heading, useColorMode } from "@chakra-ui/react";
+import { SunIcon, MoonIcon } from "@chakra-ui/icons";
 import { getMarsWeather, Sol } from "../api";
 import WeatherTable from "./WeatherTable";
 
 const App = () => {
   const [data, setData] = useState<Sol[]>([]);
+  const { colorMode, toggleColorMode } = useColorMode();
 
   useEffect(() => {
     getMarsWeather().then((res) =>
@@ -13,17 +15,23 @@ const App = () => {
   }, []);
 
   return (
-    <Grid height="100vh" justifyContent="center" alignItems="center">
-      <div>
-        <Heading textAlign="center">Latest Weather on Mars</Heading>
-        <br />
-        {data.length > 0 ? (
-          <WeatherTable data={data} />
-        ) : (
-          <Heading size="md">Loading...</Heading>
-        )}
-      </div>
-    </Grid>
+    <Fragment>
+      <Button onClick={toggleColorMode} pos="absolute" top={8} right={8}>
+        {colorMode === "light" ? <MoonIcon /> : <SunIcon />}
+      </Button>
+
+      <Grid height="100vh" justifyContent="center" alignItems="center">
+        <div>
+          <Heading textAlign="center">Latest Weather on Mars</Heading>
+          <br />
+          {data.length > 0 ? (
+            <WeatherTable data={data} />
+          ) : (
+            <Heading size="md">Loading...</Heading>
+          )}
+        </div>
+      </Grid>
+    </Fragment>
   );
 };
 
