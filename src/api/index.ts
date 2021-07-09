7c44f9cde139d55a49e8e86ea655eda8d8574b97
Sean O'Connor <seanoconnor941@gmail.com>
2021-07-09 10:52:53 -0600

    Refactor api, use data from Curiosity rover

---
+++ src/api/index.ts
@@ -0,0 +1,38 @@
+import fetch from "node-fetch";
+
+const API = new URL(
+  "https://mars.nasa.gov/rss/api/?feed=weather&category=msl&feedtype=json"
+);
+
+export type Sole = {
+  terrestrial_date: string;
+  sol: string;
+  season: string;
+  min_temp: string;
+  max_temp: string;
+  pressure: string;
+  abs_humidity: string;
+  wind_speed: string;
+  wind_direction: string;
+  atmo_opacity: string;
+  sunrise: string;
+  sunset: string;
+  local_uv_irradiance_index: string;
+  min_gts_temp: string;
+  max_gts_temp: string;
+};
+
+export const getMarsWeather = (): Promise<Array<Sole>> => {
+  return new Promise(async (resolve, reject) => {
+    await fetch(API)
+      .then((res) => res.json())
+      .then((data) => {
+        resolve(data.soles as Array<Sole>);
+        return;
+      })
+      .catch((err) => {
+        reject(err);
+        return;
+      });
+  });
+};
