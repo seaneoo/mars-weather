26bf36fc57895c65e353de66e8e2187e8dd5947c
Sean O'Connor <seanoconnor941@gmail.com>
2021-07-09 11:19:22 -0600

    Install and setup Chakra UI

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import App from "./App";
-import "./custom.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import App from "./components/App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <div className="container">
+    <ChakraProvider>
       <App />
-    </div>
+    </ChakraProvider>
   </React.StrictMode>,
   document.getElementById("mars")
 );
