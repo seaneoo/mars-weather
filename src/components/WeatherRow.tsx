56cd0a3fe99940ecb80a58288b4ed632339a6c88
Sean O'Connor <seanoconnor941@gmail.com>
2021-07-09 12:29:13 -0600

    Add WeatherRow component

---
+++ src/components/WeatherRow.tsx
@@ -0,0 +1,30 @@
+import { Td, Tr } from "@chakra-ui/react";
+import { format, parse } from "date-fns";
+import { Sol } from "../api";
+
+type Props = {
+  data: Sol;
+};
+
+const WeatherRow = ({ data }: Props) => {
+  const getTerrestrialDate = () => {
+    const d = parse(data.terrestrial_date, "yyyy-MM-dd", new Date());
+    return format(d, "MMMM d, yyyy");
+  };
+
+  const getAvgTemp = () => {
+    const min = parseInt(data.min_temp),
+      max = parseInt(data.max_temp);
+    return (min + max) / 2;
+  };
+
+  return (
+    <Tr>
+      <Td>{getTerrestrialDate()}</Td>
+      <Td>{data.sol}</Td>
+      <Td>{getAvgTemp()}</Td>
+    </Tr>
+  );
+};
+
+export default WeatherRow;
