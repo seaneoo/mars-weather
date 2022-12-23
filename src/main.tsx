004bf3269e91100029aff478836fe435be36d2be
Sean O'Connor <so@seano.dev>
2022-12-23 16:11:58 -0700

    Use normalize.css and create global styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import { ApiProvider } from "@reduxjs/toolkit/dist/query/react";
+import "normalize.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { NASA } from "./api";
 import App from "./App";
+import GlobalStyles from "./globalStyles";
 
 ReactDOM.createRoot(document.getElementById("app") as HTMLElement).render(
   <React.StrictMode>
     <ApiProvider api={NASA}>
+      <GlobalStyles />
       <App />
     </ApiProvider>
   </React.StrictMode>
