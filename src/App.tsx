177470ef29f407adfb9030754441916a1da2370d
Sean O'Connor <so@seano.dev>
2022-12-23 17:06:22 -0700

    Add page heading

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,22 @@ const _Main = styled.main`
   align-items: center;
 `;
 
+const _Title = styled.h1`
+  display: inline-block;
+  color: #f6d365;
+  background: linear-gradient(120deg, #f6d365 0%, #fda085 100%);
+  -webkit-background-clip: text;
+  background-clip: text;
+  -webkit-text-fill-color: transparent;
+`;
+
 function App() {
   return (
     <_Main>
-      <WeatherTable />
+      <div>
+        <_Title>Last 7 Days at the Gale Crater</_Title>
+        <WeatherTable />
+      </div>
     </_Main>
   );
 }
